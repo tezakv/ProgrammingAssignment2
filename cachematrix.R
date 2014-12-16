6230ed56a787f0ccbb1f5e89010201c69c0b76bb
tezakv <tezakv@gmail.com>
2014-12-16 22:04:33 +0100

    Matrix inverse to/from cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Solution for fetching the inverse of a matrix from cache
 
-## Write a short comment describing this function
+## makeCacheMatrix creates 'matrix' that stores its inversion and provides it upon request
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(mat = matrix()) {
+
+    inverseMatrix <- NULL
+    
+    set <- function(applyMatrix) {
+        mat <<- applyMatrix
+        inverseMatrix <<- NULL
+    }
+    
+    get <- function() mat
+    
+    setInverse <- function(inv) inverseMatrix <<- inv
+    
+    getInverse <- function() inverseMatrix
+    
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the inverse of a matrix if the matrix is 'new' or 'changed', else fetches the inverse from cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(invMatClass, ...) {
+    
+	## Return a matrix that is the inverse of 'mat'
+		
+	inverseMatrix <- invMatClass$getInverse()
+    
+    if (!is.null(inverseMatrix)) {
+        message("Fetching inverse from cache...")
+        return(inverseMatrix)       
+    }
+    
+    matrix <- invMatClass$get()
+    
+    inverseMatrix <- solve(matrix, ...)
+    
+    invMatClass$setInverse(inverseMatrix)
+    
+    inverseMatrix    
 }
